c221ae3aa22b0a285cae7a6df0bad9e45c172b1e
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-13 18:28:04 +0530

    Feat: task10 - implement publisher messages.

---
+++ main10.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from rabbitmq_t10 import RabbitmqConnectionTask10
+from publisher_t10 import RabbitmqPublisherTask10
+
+async def main10():
+
+    rabbitmq = RabbitmqConnectionTask10("amqp://guest:guest@localhost:5672")
+    await rabbitmq.connect()
+    await rabbitmq.channel.declare_queue(name="file_queue", durable=True)
+    publisher = RabbitmqPublisherTask10(exchange=rabbitmq.exchange)
+    for i in range(1,11):
+        await publisher.publish({"event": "file.uploaded", "file_id": i})
+    asyncio.sleep(2)
+    await rabbitmq.close()
+
+if __name__ == "__main__":
+    asyncio.run(main10())
