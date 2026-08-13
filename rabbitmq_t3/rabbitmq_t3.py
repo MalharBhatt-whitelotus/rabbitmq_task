1bbfdc92b6d490d536f5faa89ac264ced3b26a19
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-13 18:09:03 +0530

    Feat: task3 - implement rabbitmq connection.

---
+++ rabbitmq_t3/rabbitmq_t3.py
@@ -0,0 +1,19 @@
+import aio_pika
+
+class RabbitmqConnectionTask3:
+
+    def __init__(self, url: str):
+        self.url = url
+        self.connection = None
+        self.channel = None
+
+    async def connect(self):
+        self.connection = await aio_pika.connect(self.url)
+        self.channel = await self.connection.channel()
+
+        print("Rabbitmq task3 is connected.")
+
+    async def close(self):
+        if self.connection:
+            await self.connection.close()
+            print("Rabbitmq task3 is closed.")
