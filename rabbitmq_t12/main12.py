b7abed3abdf8da5324321386cec818fd8f749fae
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-14 13:07:22 +0530

    Fix: Rabbitmq url

--- rabbitmq_t12/main12.py
+++ rabbitmq_t12/main12.py
@@ -4,7 +4,7 @@ from rabbitmq_t12 import RabbitmqConnectionTask12
 from publisher_t12 import RabbitmqPublisherTask12
 
 async def main12():
-    rabbitmq = RabbitmqConnectionTask12("ampq://guest:guest@localhost:5672")
+    rabbitmq = RabbitmqConnectionTask12("amqp://guest:guest@localhost:5672")
     await rabbitmq.connect()
     await rabbitmq.channel.declare_queue("main_queue", durable=True)
 
