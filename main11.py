ba70f1e7467a8e32bd872bf2d93092028080019c
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-14 10:53:37 +0530

    Feat: task11 - implement publisher messages.

---
+++ main11.py
@@ -0,0 +1,16 @@
+import asyncio
+
+from rabbitmq_t11 import RabbitmqConnectionTask11
+from publisher_t11 import RabbitmqPublisherTask11
+
+async def main11():
+    rabbitmq = RabbitmqConnectionTask11("amqp://guest:guest@localhost:5672")
+    await rabbitmq.connect()
+    await rabbitmq.channel.declare_queue(name="file_queue", durable=True)
+    publisher = RabbitmqPublisherTask11(rabbitmq.exchange)
+    await publisher.publish({"event": "file.uploaded", "file_id": 1, "retry_count":1})
+    await asyncio.sleep(2)
+    await rabbitmq.close()
+
+if __name__ == "__main__":
+    asyncio.run(main11())
