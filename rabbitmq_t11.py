f9f7b308e6128b18c839f5d30049df5330cf39f9
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-14 10:53:17 +0530

    Feat: task11 - implement rabbitmq connection.

---
+++ rabbitmq_t11.py
@@ -0,0 +1,22 @@
+import aio_pika
+
+class RabbitmqConnectionTask11:
+
+    def __init__(self, url):
+        self.url = url
+        self.connection = None
+        self.channel = None
+        self.exchange = None
+        self.dlq_exchange = None
+
+    async def connect(self):
+        self.connection = await aio_pika.connect_robust(self.url)
+        self.channel = await self.connection.channel()
+        self.exchange = await self.channel.declare_exchange(name="file_exchange", type=aio_pika.ExchangeType.DIRECT, durable=True)
+        self.dlq_exchange = await self.channel.declare_exchange(name="dlq_exchange", type=aio_pika.ExchangeType.DIRECT, durable=True)
+        print("Rabbitmq task11 connected...")
+
+    async def close(self):
+        if self.connection:
+            await self.connection.close()
+            print("Rabbitmq task11 closed...")
