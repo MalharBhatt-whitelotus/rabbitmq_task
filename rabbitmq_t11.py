f5460467e06526e4e9fcc4d70aaa8d245e1dc24a
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-14 11:32:52 +0530

    Fix: Implement dlq correctly.

--- rabbitmq_t11.py
+++ rabbitmq_t11.py
@@ -8,12 +8,15 @@ class RabbitmqConnectionTask11:
         self.channel = None
         self.exchange = None
         self.dlq_exchange = None
+        self.dlq = None
 
     async def connect(self):
         self.connection = await aio_pika.connect_robust(self.url)
         self.channel = await self.connection.channel()
         self.exchange = await self.channel.declare_exchange(name="file_exchange", type=aio_pika.ExchangeType.DIRECT, durable=True)
         self.dlq_exchange = await self.channel.declare_exchange(name="dlq_exchange", type=aio_pika.ExchangeType.DIRECT, durable=True)
+        self.dlq = await self.channel.declare_queue(name="dlq", durable=True,)
+        await self.dlq.bind(exchange=self.dlq_exchange, routing_key="failed")
         print("Rabbitmq task11 connected...")
 
     async def close(self):
