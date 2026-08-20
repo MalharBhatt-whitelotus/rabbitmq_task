582c17c6a429ebc43c4ed6f94b13bd00baf37d62
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-20 14:49:36 +0530

    Feat: Task24- implement consumer worker 1.

---
+++ consumer_worker1_t24.py
@@ -0,0 +1,9 @@
+import asyncio
+from consumer_t24 import RabbitmqConsumerTask24
+if __name__ == "__main__":
+    asyncio.run(
+        RabbitmqConsumerTask24(
+            url="amqp://guest:guest@localhost:5672",
+            con_num=1,
+        ).main()
+    )
