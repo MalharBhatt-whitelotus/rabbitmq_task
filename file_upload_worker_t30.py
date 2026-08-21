3510f2393f6247076e8720ce73a04991f3d6c008
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-21 18:41:08 +0530

    Feat: Task30- implement file upload worker.

---
+++ file_upload_worker_t30.py
@@ -0,0 +1,10 @@
+import asyncio
+from consumer_t30 import RabbitmqConsumerTask30
+
+if __name__ == "__main__":
+    asyncio.run(
+        RabbitmqConsumerTask30(
+            "amqp://guest:guest@localhost:5672",
+            "file_upload"
+        ).main()
+    )
